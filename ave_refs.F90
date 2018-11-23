23cc0c025c8bd3ef2b3573d2676cf10cf88a9e5b
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2018-11-23 12:06:56 +0900

    Create ave_refs.F90

---
+++ ave_refs.F90
@@ -0,0 +1,32 @@
+program main
+  integer, parameter :: ave_num = 30, row_num = 1, d = 1396
+  real(8) :: x(ave_num, row_num, d, d), ave(row_num, d, d) = 0.0e0
+  character filename*128
+  integer :: unit_num, row, j, k, l
+
+  do j = 1, ave_num
+    do row = 1, row_num
+      unit_num = 30*row_num+j
+      write (filename, '("ERmod_", i4.4, "/refs/corref.", i2.2)') j,row
+      open (unit = unit_num, file = filename, form = "UNFORMATTED", action ="read", status = "OLD")
+      read (unit = unit_num) x(j, row, :, :)
+    end do
+  end do
+
+  do k = 1, d
+    do l = 1, d
+      do row = 1, row_num
+        do j = 1, ave_num
+          ave(row, k, l) = ave(row, k, l) + x(j, row, k, l)
+        end do
+        ave(row, k, l) = ave(row, k, l) / dble(ave_num)
+        open(unit = cor_io, file =
+        print *, k, l, ave(row, k, l)
+      end do
+    end do
+  end do
+
+
+
+
+end program main
