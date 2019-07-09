8784ed4166fc3d49a8fc55f42de1651a343ccab6
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2019-07-09 18:19:06 +0900

    Update calc_sigma.f90

--- calc_sigma.f90
+++ calc_sigma.f90
@@ -1,19 +1,21 @@
- program calc_sigma
+program calc_sigma
   implicit none
   integer i, i0, j, k
   double precision, allocatable,dimension(:,:) :: P
-  double precision, allocatable,dimension(:) :: t
+  double precision, allocatable,dimension(:) :: t, Geta
   integer n
   double precision, allocatable,dimension(:,:) :: G
   n = num
+
   allocate( P(3,n) )
   allocate( t(n) )
   allocate( G(3,0:n) )
+  allocate( Geta(0:n) )
   G = 0.0
 
   open(99, file='sigma.xvg' )
   do j = 1,26
-  read (99,'()')
+  read (99, *)
   end do
 
   do i = 1, n
@@ -23,10 +25,10 @@
 
 !$omp parallel
 !$omp do
-  do i = 0, n
+  do i = 0, n-1
     do k = 1,3
       do i0 = 1, n-i
-        G(k,i) = G(k,i) + P(k,i+i0-1)*P(k,i0)
+        G(k,i) = G(k,i) + P(k,i+i0)*P(k,i0)
       end do
       G(k,i) = G(k,i)/(n-i+1)
     end do
@@ -35,22 +37,34 @@
 !$omp end parallel
 
   open(98, file='Gxy.xvg')
-  do i = 0, n
+  do i = 0, n-1
     write(98, "(f9.4, f20.11)") t(i), G(1,i)
   end do
   close(98)
 
   open(97, file='Gxz.xvg')
-  do i = 0, n
+  do i = 0, n-1
     write(97, "(f9.4, f20.11)") t(i), G(2,i)
   end do
   close(97)
 
   open(96, file='Gyz.xvg')
-  do i = 0, n
+  do i = 0, n-1
     write(96, "(f9.4, f20.11)") t(i), G(3,i)
   end do
   close(96)  
 
- 
+do i = 0, n-1
+
+  do k=1,3
+    Geta(i) = Geta(i) + G(k,i)
+  end do
+  Geta(i) = Geta(i)/dble(3.0)
+end do
+
+  open(95, file='Geta.xvg')
+  do i = 0, n-1
+    write(95, "(f9.4, f20.11)") t(i), Geta(i)
+  end do 
+  close(95)
 end
