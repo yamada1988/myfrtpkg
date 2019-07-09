9246a3247859fa30beb8c04e378a3e439fb14a61
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2019-07-09 15:44:22 +0900

    Update calc_sigma.f90

--- calc_sigma.f90
+++ calc_sigma.f90
@@ -1,4 +1,4 @@
-program calc_sigma
+ program calc_sigma
   implicit none
   integer i, i0, j, k
   double precision, allocatable,dimension(:,:) :: P
@@ -8,7 +8,7 @@ program calc_sigma
   n = num
   allocate( P(3,n) )
   allocate( t(n) )
-  allocate( G(3,n) )
+  allocate( G(3,0:n) )
   G = 0.0
 
   open(99, file='sigma.xvg' )
@@ -21,30 +21,33 @@ program calc_sigma
   end do
   close(99)
 
-  do i = 1, n
+!$omp parallel
+!$omp do
+  do i = 0, n
     do k = 1,3
       do i0 = 1, n-i
-        G(k,i) = G(k,i) + P(k,i+i0)*P(k,i0)
+        G(k,i) = G(k,i) + P(k,i+i0-1)*P(k,i0)
       end do
       G(k,i) = G(k,i)/(n-i+1)
     end do
   end do
-
+!$omp end do
+!$omp end parallel
 
   open(98, file='Gxy.xvg')
-  do i = 1, n
+  do i = 0, n
     write(98, "(f9.4, f20.11)") t(i), G(1,i)
   end do
   close(98)
 
   open(97, file='Gxz.xvg')
-  do i = 1, n
+  do i = 0, n
     write(97, "(f9.4, f20.11)") t(i), G(2,i)
   end do
   close(97)
 
   open(96, file='Gyz.xvg')
-  do i = 1, n
+  do i = 0, n
     write(96, "(f9.4, f20.11)") t(i), G(3,i)
   end do
   close(96)  
