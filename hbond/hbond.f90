57d37fd5858aa53cbca4935399b4da0db560326f
Kazuo Yamada <k.yamada@cheng.es.osaka-u.ac.jp>
2019-07-11 16:38:19 +0900

    add make_list function

--- hbond/hbond.f90
+++ hbond/hbond.f90
@@ -14,8 +14,11 @@ program hbond
     double precision ::  dt
     double precision, allocatable, dimension(:) :: box
     integer :: it, pit, pit0, totalstep, skipstep, ptotalstep
-    integer :: i,j,k, jo, jh1, jh2
+    integer :: i,j,k, jo, jh1, jh2, ik
     character outf*128
+  
+    real(8) :: dist
+    real(8), parameter :: dlist = 0.60
 
     type mytype
     real(8), dimension(3):: O
@@ -25,6 +28,13 @@ program hbond
 
     type(mytype), allocatable, dimension(:,:) :: tip4p
 
+    type mytype2
+    integer, dimension(50) :: pair = -1
+    end type mytype2
+
+    type(mytype2), allocatable, dimension(:) :: DistanceList
+
+    
 
     ! 2. Declare a variable of type xtcfile
     type(xtcfile) :: xtcf
@@ -41,25 +51,11 @@ program hbond
     ptotalstep = totalstep / skipstep
     allocate(tip4p(0:ptotalstep, 1:no))
     allocate(box(0:ptotalstep))
- 
-    ! step = 0
-    it = 0
-    pit = 0
-    call xtcf % read
-    do i=1, no
-      jo = 4*(i-1) + 1
-      jh1 = jo + 1
-      jh2 = jo + 2 
-      tip4p(pit,i) % O = xtcf % pos(:, jo)
-      tip4p(pit,i) % H1 = xtcf % pos(:, jh1)
-      tip4p(pit,i) % H2 = xtcf % pos(:, jh2)  
-    end do
-    box(pit) = xtcf % box(1,1)
-    call xtcf % read
-    dt = xtcf % time
-    it = it + 1
-    pit = pit + 1
+    allocate(DistanceList(1:no))
 
+    call xtcf % read
+    it = 0 
+    pit = 0
     ! read trajectory
     do while ( xtcf % STAT == 0 )
         if (it > totalstep) exit
@@ -71,15 +67,21 @@ program hbond
           cycle
         end if 
 
+        ! output log
         if (mod(it, 10000) == 0) then
           write(*,'(a,f12.6,a,i0)') " Time (ps): ", xtcf % time, "  Step: ", xtcf % STEP
+        end if
+
+        ! get descrete time
+        if (it == 1) then
+          dt = xtcf % time
         end if
 
         ! get box in it-th step
         box(pit) = xtcf % box(1,1)
 
         !$omp parallel shared(xtcf, tip4p)
-        !$omp private(jo, jh1, jh2, box,)
+        !$omp private(jo, jh1, jh2)
         !$omp do
         ! get position (j-th O) and box at it-th step
         do i=1, no
@@ -102,6 +104,26 @@ program hbond
     ! 5. Close the file
     call xtcf % close
  
-    print *, tip4p
+
+
+    ! calculate Hydrogen bond pair
+    do pit = 1, ptotalstep
+
+      ! make list
+      if (mod(pit, 10) == 0) then
+        do i=1, no
+          ik = 1
+          do j=1, no
+            dist = sqrt(sum((tip4p(pit,i)%O - tip4p(pit,j)%O)**2))
+            if (dist <= dlist ) then
+              DistanceList(i)%pair(ik) = j
+              ik = ik + 1 
+            end if
+          end do
+          print *, pit, i, DistanceList(i)%pair
+        end do
+      end if
+
+    end do
 
 end program hbond
